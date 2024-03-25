17262ce3b99bee416232ac5e4eac4c7164c2717f
stefanos_pan <stefanos2004@hotmail.gr>
2024-03-25 19:53:39 +0200

    more favicon stuff

--- application.py
+++ application.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/favicon.ico')
 def favicon():
-    return '', 204
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 # Configure the database URI from the environment variable
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
