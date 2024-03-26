5c7c478a883b21078cd6a80109872212c1621c3f
stefanos_pan <stefanos2004@hotmail.gr>
2024-03-26 02:50:12 +0200

    readded db code

--- application.py
+++ application.py
@@ -9,24 +9,24 @@ def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 # Configure the database URI from the environment variable
-#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 
 # Suppress deprecation warnings
-#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Initialize SQLAlchemy
-#db = SQLAlchemy(app)
+db = SQLAlchemy(app)
 
 # Define a simple model
-#class User(db.Model):
-#    id = db.Column(db.Integer, primary_key=True)
-#    username = db.Column(db.String(80), unique=True, nullable=False)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
 
-#    def __repr__(self):
-#        return '<User %r>' % self.username
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 # Create the database tables (if they don't exist)
-#db.create_all()
+db.create_all()
 
 @app.route('/')
 def index():
