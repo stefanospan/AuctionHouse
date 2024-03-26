29994bb408b495de2acdc5c01e3e3a3cd1ee04b8
stefanos_pan <stefanos2004@hotmail.gr>
2024-03-26 03:55:32 +0200

    added password

--- application.py
+++ application.py
@@ -29,6 +29,7 @@ db = SQLAlchemy(app)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
 
     def __repr__(self):
         return '<User %r>' % self.username
