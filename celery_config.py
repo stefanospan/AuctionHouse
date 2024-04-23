25933138f04fc61c297d3f82ecf8605d2893957b
stefanos_pan <stefanos2004@hotmail.gr>
2024-04-23 18:37:39 +0300

    change url

--- celery_config.py
+++ celery_config.py
@@ -2,6 +2,6 @@ import os
 from celery import Celery
 
 # Get Redis URL from environment variable (set on Heroku)
-redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379/0')
+redis_url = os.getenv('REDIS_TLS_URL', 'redis://localhost:6379/0')
 
 celery = Celery(__name__, broker=redis_url, backend=redis_url)
