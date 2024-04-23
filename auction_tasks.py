32b20ac46f3c63d35915247694dcba11965211b1
stefanos_pan <stefanos2004@hotmail.gr>
2024-04-23 18:25:28 +0300

    update

--- auction_tasks.py
+++ auction_tasks.py
@@ -2,6 +2,7 @@ from datetime import datetime, timedelta
 import logging
 from application import db, Auction, User, UserInventory  # Adjust import based on your project structure
 from celery import Celery
+from celery_config import *
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
