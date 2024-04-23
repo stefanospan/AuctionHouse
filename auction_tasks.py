e21d6dd930cf8cff5450aaf996963f80f5c2ef17
stefanos_pan <stefanos2004@hotmail.gr>
2024-04-23 18:46:22 +0300

    updates

--- auction_tasks.py
+++ auction_tasks.py
@@ -2,14 +2,11 @@ from datetime import datetime, timedelta
 import logging
 from application import db, Auction, User, UserInventory  # Adjust import based on your project structure
 from celery import Celery
-from celery_config import *
+from celery_config import celery
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-
-# Create Celery instance
-celery = Celery(__name__)
 
 @celery.task
 def process_expired_auctions():
